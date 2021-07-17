794fe84bc4329dc94cad61137a1d9e862bb53eba
JoannaCoPo <jococopo@gmail.com>
2021-07-17 15:04:11 -0600

    Add pagination functionality to index action

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,5 +1,15 @@
 class Api::V1::MerchantsController < ApplicationController
+  MERCHANTS_PER_PAGE = 20
+
   def index
-    render json: Merchant.all
+    @page = params.fetch(:page, 1).to_i
+    @merchants = Merchant.offset(@page * MERCHANTS_PER_PAGE).limit(MERCHANTS_PER_PAGE)
+     # ^ page num * merchants per page
+    # merchants = Merchant.limit(per_page).offset(page)
+    render json: MerchantSerializer.new(@merchants)
   end
+
+  private
+
+
 end
