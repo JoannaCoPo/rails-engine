18237475845629aa2ccea1d630e1d2feae7e72e3
JoannaCoPo <jococopo@gmail.com>
2021-07-17 16:11:08 -0600

    Move pagination functionality to module

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,15 +1,8 @@
 class Api::V1::MerchantsController < ApplicationController
-  MERCHANTS_PER_PAGE = 20
+  include Paginator
 
   def index
-    @page = params.fetch(:page, 1).to_i
-    @merchants = Merchant.offset(@page * MERCHANTS_PER_PAGE).limit(MERCHANTS_PER_PAGE)
-     # ^ page num * merchants per page
-    # merchants = Merchant.limit(per_page).offset(page)
-    render json: MerchantSerializer.new(@merchants)
+    @merchants = Merchant.offset(page).limit(per_page)
+    render json: MerchantsSerializer.new(@merchants)
   end
-
-  private
-
-
 end
