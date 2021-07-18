901dfa12c694f59fcb2c9cb9a8cce21bcdd6f35d
JoannaCoPo <jococopo@gmail.com>
2021-07-18 09:48:18 -0600

    Create items request test file

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -2,27 +2,9 @@ require 'rails_helper'
 
 describe "Items API", type: :request do
   describe 'happy path' do
-    it "gets all merchants" do
-      create_list(:item, 3)
+    it '' do
 
-      get '/api/v1/items'
-
-      expect(response).to be_successful
-      expect(response.status).to eq(200)
-      expect(response.server_error?).to eq(false)
-
-      items = JSON.parse(response.body, symbolize_names: true)
-
-      expect(items[:data].count).to eq(3)
-
-      items[:data].each do |item|
-        expect(item[:attributes]).to have_key(:name)
-        expect(item[:attributes][:name]).to be_a(String)
-        expect(item[:attributes]).to have_key(:description)
-        expect(item[:attributes][:description]).to be_a(String)
-        expect(item[:attributes]).to have_key(:unit_price)
-        expect(item[:attributes][:unit_price]).to be_a(Float)
-      end
+    
     end
   end
 end
