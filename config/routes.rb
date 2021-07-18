b0c327c87d4e2365fac19c20c6a33ae4cb92a226
JoannaCoPo <jococopo@gmail.com>
2021-07-18 09:52:42 -0600

    Fix routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :merchants, only: [:index, :show] do
-        resources :items, only: [:index]
-      end
+      resources :merchants, only: [:index, :show]
+        # resources :items, only: [:index]
+      get "merchants/:id/items", to: 'merchant_items#index'  
     end
   end
 end
