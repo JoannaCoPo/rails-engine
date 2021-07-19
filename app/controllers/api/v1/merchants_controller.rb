9bf7ff993998053e27855fe90bf4c3fb5896938f
JoannaCoPo <jococopo@gmail.com>
2021-07-19 14:17:51 -0600

    Add serializer to show action

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -2,11 +2,12 @@ class Api::V1::MerchantsController < ApplicationController
   include Paginator
 
   def index
-    @merchants = Merchant.offset(page).limit(per_page)
-    render json: MerchantSerializer.new(@merchants)
+    merchants = Merchant.offset(page).limit(per_page)
+    render json: MerchantSerializer.new(merchants)
   end
 
   def show
-    render json: Merchant.find(params[:id])
+    merchant = Merchant.find(params[:id])
+    render json: MerchantSerializer.new(merchant)
   end
 end
