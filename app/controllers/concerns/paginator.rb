26d0adf83971c245964958735b586cb43066805b
JoannaCoPo <jococopo@gmail.com>
2021-09-04 17:45:34 -0600

    Move Paginator to concerns directory.

---
+++ app/controllers/concerns/paginator.rb
@@ -0,0 +1,15 @@
+module Paginator
+  MERCHANTS_PER_PAGE = 20
+
+  def per_page
+    params.fetch(:per_page, MERCHANTS_PER_PAGE).to_i
+  end
+
+  def page
+     page = [params.fetch(:page, 1).to_i, 1].max
+    # page = params.fetch(:page, 1).to_i
+    (page - 1) * per_page
+  end
+    # ^ page num * merchants per page
+    # merchants = Merchant.limit(per_page).offset(page)
+end
