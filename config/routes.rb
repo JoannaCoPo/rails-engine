6bd3bbd77de66f8c1c06612faa4b540e88e01d68
JoannaCoPo <jococopo@gmail.com>
2021-09-04 21:19:01 -0600

    Fix rubocop offenses.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :merchants, only: [:index, :show]
-        # resources :items, only: [:index]
+      resources :merchants, only: %i[index show]
+      # resources :items, only: [:index]
       get 'merchants/:id/items', to: 'merchants/items#index'
     end
   end
