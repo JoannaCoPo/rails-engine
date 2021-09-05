e7ffcb0d0de31ea783e2c81de4357448138a7d2d
JoannaCoPo <jococopo@gmail.com>
2021-09-04 21:14:25 -0600

    Fix rubocop offense.

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,12 +1,18 @@
-class Api::V1::MerchantsController < ApplicationController
-  def index
-    merchants = Merchant.offset(page).limit(per_page)
-    # render json: MerchantSerializer.new(merchants)
-    json_response(MerchantSerializer.new(merchants))
-  end
+# frozen_string_literal: true
 
-  def show
-    merchant = Merchant.find(params[:id])
-    json_response(MerchantSerializer.new(merchant))
+module Api
+  module V1
+    class MerchantsController < ApplicationController
+      def index
+        merchants = Merchant.offset(page).limit(per_page)
+        # render json: MerchantSerializer.new(merchants)
+        json_response(MerchantSerializer.new(merchants))
+      end
+
+      def show
+        merchant = Merchant.find(params[:id])
+        json_response(MerchantSerializer.new(merchant))
+      end
+    end
   end
 end
