75baa3babe9f84ab63d0b2a6d0ab7dcd77aa0df9
JoannaCoPo <jococopo@gmail.com>
2021-09-04 21:14:20 -0600

    Fix rubocop offense.

--- app/controllers/api/v1/merchants/items_controller.rb
+++ app/controllers/api/v1/merchants/items_controller.rb
@@ -1,6 +1,14 @@
-class Api::V1::Merchants::ItemsController < ApplicationController
-  def index
-    merchants = Merchant.find(params[:id])
-    json_response(ItemSerializer.new(merchants.items))
+# frozen_string_literal: true
+
+module Api
+  module V1
+    module Merchants
+      class ItemsController < ApplicationController
+        def index
+          merchants = Merchant.find(params[:id])
+          json_response(ItemSerializer.new(merchants.items))
+        end
+      end
+    end
   end
 end
