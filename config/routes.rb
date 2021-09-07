b2807f49f6edb440f8c223f09337aa69030a0d00
JoannaCoPo <jococopo@gmail.com>
2021-09-06 23:54:01 -0600

    Add non-restful for merchant search; adjust order.

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
+      get 'merchants/:id/items', to: 'merchants/items#index'
+      get 'merchants/find', to: 'merchants/search#find'
+      
       resources :items, only: %i[index create]
       resources :merchants, only: %i[index show]
       # resources :items, only: [:index]
-      get 'merchants/:id/items', to: 'merchants/items#index'
+      # get 'merchants/:id/items', to: 'merchants/items#index'
+      # get 'merchants/find', to: 'merchants/search#find'
+
     end
   end
 end
