58e4c9be553ceff5e5fac6a2286b8284e163f023
JoannaCoPo <jococopo@gmail.com>
2021-09-07 17:28:21 -0600

    Incorporate search_error and return first object.

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -1,5 +1,13 @@
 class Api::V1::Merchants::SearchController < ApplicationController
   def find
-
+    merchant_results = Merchant.search_merchant_with_query(params[:name])
+    return search_error if merchant_results.empty?
+    json_response(MerchantSerializer.new(merchant_results.first))
   end
 end
+
+# return the first object in the database in case-sensitive alphabetical order
+# if multiple matches are found
+
+#return the first object in the database in case-sensitive alphabetical order if
+# multiple matches are found
