4d6ac96bcb78c613b4049ce5d479593d409d3e08
JoannaCoPo <jococopo@gmail.com>
2021-09-07 17:31:48 -0600

    Clean up routes file.

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      get 'merchants/:id/items', to: 'merchants/items#index'
       get 'merchants/find', to: 'merchants/search#find'
-      
+      get 'merchants/:id/items', to: 'merchants/items#index'
+
       resources :items, only: %i[index create]
       resources :merchants, only: %i[index show]
       # resources :items, only: [:index]
