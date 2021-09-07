9c47487eb715d83cec6aad2c700253f9fdb62759
JoannaCoPo <jococopo@gmail.com>
2021-09-07 17:30:33 -0600

    Create response for 400 status.

--- app/controllers/concerns/response.rb
+++ app/controllers/concerns/response.rb
@@ -4,4 +4,8 @@ module Response
   def json_response(object, status = :ok)
     render json: object, status: status
   end
+
+  def search_error
+    render json: { error: 'Invalid request' }, status: 400
+  end
 end
