0ff2851755160ed2ebe563a19840200a4dfb5f44
JoannaCoPo <jococopo@gmail.com>
2021-09-07 17:27:30 -0600

    Adjust assertions.

--- spec/requests/api/v1/merchants/search_spec.rb
+++ spec/requests/api/v1/merchants/search_spec.rb
@@ -9,20 +9,15 @@ describe 'Merchants Search', type: :request do
       merchant_2 = create(:merchant, name: 'Klein, Rempel and Jones')
       merchant_3 = create(:merchant, name: 'Willms and Sons')
 
-      get '/api/v1/merchants/find', params: { name: 'reMp'}
+      get "/api/v1/merchants/find/", params: { name: 'emPe'}
 
-      # expect(response).to be_successful
-      # expect(response.status).to eq(200)
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
 
       search_result = JSON.parse(response.body, symbolize_names: true)
-require "pry"; binding.pry
-      expect(search_result[:data]).to be_a(Hash)
-      expect(search_result[:data][:id]).to eq(merchant_2.id)
 
-      # search_result[:data].each do |result|
-      #   expect(result[:attributes]).to have_key(:name)
-      #   expect(result[:attributes][:name]).to be_a(String)
-      # end
+      expect(search_result).to be_a(Hash)
+      expect(search_result[:data][:id]).to eq(merchant_2.id.to_s)
     end
   end
 end
