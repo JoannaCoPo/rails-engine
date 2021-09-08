50eabbb0dc0311ed07a85a2f5ae25d474cd64338
JoannaCoPo <jococopo@gmail.com>
2021-09-07 20:34:02 -0600

    Add class search method to item model.

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,9 @@ class Item < ApplicationRecord
   has_many :invoice_items, dependent: :destroy
 
   validates :name, :description, :unit_price, presence: true
+
+  def self.search_with_query(query) #does this need to be alphabetical
+    # order(:name).where('name ILIKE ?', "%#{query}%")
+    where('name ILIKE ?', "%#{query}%")
+  end
 end
