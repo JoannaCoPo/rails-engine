3b30528e1ca60317ed301c5a4ec81a1a302f6d0c
JoannaCoPo <jococopo@gmail.com>
2021-09-07 18:44:57 -0600

    Change merchant_results to nil?.

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::Merchants::SearchController < ApplicationController
   def find
     merchant_results = Merchant.search_merchant_with_query(params[:name])
-    return no_match if merchant_results.empty?
-    json_response(MerchantSerializer.new(merchant_results.first))
+    return no_match if merchant_results.nil?
+    json_response(MerchantSerializer.new(merchant_results))
   end
 end
 
