949ed90aaaca5890362997bced73d823f02358f0
JoannaCoPo <jococopo@gmail.com>
2021-09-07 18:04:11 -0600

    Adjust naming of helper response method.

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::Merchants::SearchController < ApplicationController
   def find
     merchant_results = Merchant.search_merchant_with_query(params[:name])
-    return search_error if merchant_results.empty?
+    return no_match if merchant_results.empty?
     json_response(MerchantSerializer.new(merchant_results.first))
   end
 end
