07b07956f677490634c9aee2326f0555cd9b323a
JoannaCoPo <jococopo@gmail.com>
2021-09-07 20:34:33 -0600

    Create routing for item find_all action.

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get 'merchants/find', to: 'merchants/search#find'
+      get 'items/find_all', to: 'items/search#find_all'
+      
       get 'merchants/:id/items', to: 'merchants/items#index'
 
       resources :items, only: %i[index create]
