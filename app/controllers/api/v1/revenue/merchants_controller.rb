5bea70caed8d0e0f9b1985c66342c33b6c536cd5
JoannaCoPo <jococopo@gmail.com>
2021-09-09 14:56:27 -0600

    Utilize MerchantRevenueSerializer in most_revenue action.

--- app/controllers/api/v1/revenue/merchants_controller.rb
+++ app/controllers/api/v1/revenue/merchants_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::Revenue::MerchantsController < ApplicationController
   def most_revenue
-
+    merchants = Merchant.top_revenue(params[:quantity])
+    json_response(MerchantRevenueSerializer.new(merchants))
   end
 end
