356b504db3fb19ee221c90165d31687c5c231d27
JoannaCoPo <jococopo@gmail.com>
2021-09-09 14:32:43 -0600

    Pseudocode top revenue class method.

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,11 +3,26 @@
 class Merchant < ApplicationRecord
   has_many :items, dependent: :destroy
   has_many :invoices
+  # has_many :invoice_items, through: :items
+  # has_many :transactions, through: :invoice_items
 
   def self.search_merchant_with_query(query)
     order(:name).where('name ILIKE ?', "%#{query}%").first
   end
+
+# revenue = Number of units sold * average price.
+
+  def top_revenue(query)
+    # joins table invoice items or invoice?
+    # access transactions for successful transaction and shipped status
+    # organize by merchant id
+    # grab all merch and set revenue as quanity * unit_price?
+    # order descending
+    # do i want to limit this?
+  end
 end
+
+# HINT: Invoices must have a successful transaction and be shipped to the customer to be considered as revenue.
 
 # ILIKE
 # Allows matching of strings based on comparison with a pattern but is
